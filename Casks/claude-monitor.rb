21a81afff014a7c097ec08cd4064e75207c00e89
Louis Volland <louis@lvolland.fr>
2026-04-17 18:59:03 +0200

    Update claude-monitor to v1.7

--- Casks/claude-monitor.rb
+++ Casks/claude-monitor.rb
@@ -1,6 +1,6 @@
 cask "claude-monitor" do
-  version "1.6"
-  sha256 "7b4cc1147ef8ceffba3eddd472d5271df1a7da50dfe6aa25c002f92555444fe0"
+  version "1.7"
+  sha256 "adc5569262e376cf22b7ca989c0022a77e26c2f1bd91962f892e8f1ea64a11fe"
 
   url "https://github.com/lvolland/claude-monitor/releases/download/v#{version}/ClaudeMonitor-v#{version}-macOS.zip"
   name "Claude Monitor"
